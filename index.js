96ed26f8a0904bfa72d694bc644e4fa108bea667
eldirb21 <eldirb95@gmail.com>
2024-09-20 13:47:58 +0700

    cresh appsenter

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { AppCenter } from 'appcenter';
+
+AppCenter.start('9a4f64c6-9b2a-4c9a-af04-91c73a1ac55e', [
+    AppCenterAnalytics,
+    AppCenterCrashes
+]);
 
 AppRegistry.registerComponent(appName, () => App);
