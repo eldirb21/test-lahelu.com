6a7d1a5abc78b2617134ad4140992bc1dd932dc8
eldirb21 <eldirb95@gmail.com>
2024-09-20 14:21:29 +0700

    update

--- ios/TestLahelu/AppDelegate.mm
+++ ios/TestLahelu/AppDelegate.mm
@@ -1,5 +1,8 @@
 #import "AppDelegate.h"
-
+#import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,6 +13,12 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  [AppCenterReactNativeShared setStartAutomatically:YES];
+  [AppCenterReactNativeShared setAppSecret:@"{9a4f64c6-9b2a-4c9a-af04-91c73a1ac55e}"];
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
