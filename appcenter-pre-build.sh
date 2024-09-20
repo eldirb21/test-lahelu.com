c1ad0de05951bfdd9c7f924409b3e581560833b2
eldirb21 <eldirb95@gmail.com>
2024-09-20 14:10:42 +0700

    cresh appsenter

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Atur versi Node.js
-nvm install 18 # Ganti dengan versi yang kamu butuhkan
-nvm use 18
+# # Atur versi Node.js
+# nvm install 18 # Ganti dengan versi yang kamu butuhkan
+# nvm use 18
 
-# Install Yarn jika diperlukan
-npm install -g yarn@3.6.4
+# # Install Yarn jika diperlukan
+# npm install -g yarn@3.6.4
