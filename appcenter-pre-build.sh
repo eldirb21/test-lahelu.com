d34c5a3a258e87d18fb6277823800608a1810368
eldirb21 <eldirb95@gmail.com>
2024-09-22 07:05:04 +0700

    update app center

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,14 +1,22 @@
-# #!/bin/bash
+# # #!/bin/bash
 
-# Set JAVA_HOME to the correct path
-export JAVA_HOME=$(/usr/libexec/java_home -v 17)
-echo "JAVA_HOME set to $JAVA_HOME"
+# # Set JAVA_HOME to the correct path
+# export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+# echo "JAVA_HOME set to $JAVA_HOME"
 
-# Install NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 18
-nvm use 18
+# # Install NVM
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# nvm install 18
+# nvm use 18
 
-# Install Yarn
-npm install -g yarn@3.6.4
+# # Install Yarn
+# npm install -g yarn@3.6.4
+
+#!/usr/bin/env bash
+
+# Example: Change bundle name of an iOS app for non-production
+if [ "$APPCENTER_BRANCH" != "main" ];
+then
+    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Production" $APPCENTER_SOURCE_DIRECTORY/TestLahelu/Info.plist
+fi
